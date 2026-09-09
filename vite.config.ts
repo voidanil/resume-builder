ffb10f0019de39827ab3ffa6c51b4c59f4eeda3c
Anil <anil72.rana@gmail.com>
2026-09-09 22:14:38 +0545

    fix: repo name issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://voidanil.github.io/resume_builder/",
+  base: "https://voidanil.github.io/resume-builder/",
 });
